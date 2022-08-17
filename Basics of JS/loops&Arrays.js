bafce39f34bb4b8795c87bdffa07e244dc7ad7e5
saseivanovski <sase.ivanovski@gmail.com>
2022-08-18 00:23:04 +0200

    js zadacki

--- Basics of JS/loops&Arrays.js
+++ Basics of JS/loops&Arrays.js
@@ -1,4 +1,6 @@
 //1
+//pravime varijabla koja sto ke ima vrednost na indeksot na elementot od nizata, sto go dobivame preku value koga ja povikuvame funkcijata
+//potoa istiot go eliminirame od nizata
 function removeNumber(arr, value) {
   let index = arr.indexOf(value);
   arr.splice(index, 1);
@@ -7,7 +9,8 @@ function removeNumber(arr, value) {
 console.log(removeNumber([1, 2, 3, 4, 5, 6], 5));
 
 //2
-//vaka bi proveril za dolzina na broj , ke pretrovram vo string i ke izbrojam dolzina - [556575757].toString().split('').length
+//vaka bi proveril za dolzina na broj , ke pretrovram vo string i ke izbrojam dolzina
+console.log([556575757].toString().split("").length);
 
 //eve so loop
 const digits = function (i) {
@@ -15,7 +18,6 @@ const digits = function (i) {
   while (i > 0) {
     i = Math.floor(i / 10); //go delime brojot so 10,decimalata ja spustame na najniskata bliska vrednost,(so sekoe delenje eliminira eden broj) i broi kolku pati ja pravi operacijata
     result++; //i vo varijablata result ni dodava +1 za sekoja gorenapravena presmetka na i
-    console.log(i);
   }
   return result;
 };
