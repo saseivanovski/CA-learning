c932e126dee1471f6a0d0f995f0b99bb87bef17e
saseivanovski <sase.ivanovski@gmail.com>
2022-08-18 00:11:08 +0200

    js zadacki

--- Basics of JS/loops&Arrays.js
+++ Basics of JS/loops&Arrays.js
@@ -13,7 +13,7 @@ console.log(removeNumber([1, 2, 3, 4, 5, 6], 5));
 const digits = function (i) {
   let result = 0;
   while (i > 0) {
-    i = Math.floor(i / 10); //go delime brojot so 10,decimalata ja spustame na najniskata bliska vrednost, i broi kolku pati ja pravi operacijata
+    i = Math.floor(i / 10); //go delime brojot so 10,decimalata ja spustame na najniskata bliska vrednost,(so sekoe delenje eliminira eden broj) i broi kolku pati ja pravi operacijata
     result++; //i vo varijablata result ni dodava +1 za sekoja gorenapravena presmetka na i
     console.log(i);
   }
@@ -27,8 +27,9 @@ let x = [2, 31, 55, 62, 93, 5, 125];
 let largestNumber = Math.max(...x); //spread operator gi zima site elementi vo nizata, math max ni ja dava najvisokata vrednost
 console.log(largestNumber);
 
-// Tuka pravime sporedba , ovoj loop ja vrti celata niza i gi stava borevite eden po eden (zamenski) vo praznata array. koga ke napravi sporedba
-// i brojot vo nizata nema da bide pomal od toj bo glavnata niza. pravi return i funckijata zavrsuva
+// Tuka pravime sporedba , ovoj loop ja vrti celata niza i gi stava borevite eden po eden (zamenski) vo praznata array.
+// se dodeka brojot od n e pogolem od toj vo nBig, ke go zameni
+//koga ke zavrsi loopot na celata niza najviskiot broj ke ostane vo nBig nizata
 let n = [2, 31, 55, 62, 93, 5, 125];
 let nBig = [];
 for (let i = 0; i < n.length; i++) {
