248b9116927a780f37b7339379acfdbdcb76f3e2
saseivanovski <sase.ivanovski@gmail.com>
2022-08-26 00:13:36 +0200

    homework

---
+++ Basics of JS/DOM.js
@@ -0,0 +1,48 @@
+//1.
+//adding new row and cells with value inside
+function adding() {
+  let table = document.querySelector("table");
+  let row = table.insertRow(-1);
+
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+  let cell3 = row.insertCell(2);
+  let cell4 = row.insertCell(3);
+
+  cell1.textContent = "4";
+  cell2.textContent = "Ivan";
+  cell3.textContent = "24";
+  cell4.textContent = "Male";
+}
+adding();
+
+//2.
+//using next element read-only property
+let list = document.querySelector("#li");
+let two = list.nextElementSibling;
+two.style.color = "red";
+let three = two.nextElementSibling;
+three.style.color = "green";
+
+//3.
+//change style to all li elements
+let ul = document.querySelectorAll("li");
+for (let i = 0; i < ul.length; i++) {
+  ul[i].style.backgroundColor = "yellow";
+}
+
+//4.
+//console log form entered data and return it to li elements
+let nameInput = document.getElementById("name");
+let surnameInput = document.getElementById("surname");
+document.querySelector("#form").addEventListener("submit", function (a) {
+  a.preventDefault();
+  console.log(nameInput.value, surnameInput.value);
+});
+//when any key is pressd - print
+let input = document.querySelector('[name="name"]');
+let printName = document.getElementById("li");
+
+input.addEventListener("keyup", function (a) {
+  printName.innerHTML = a.currentTarget.value;
+});
