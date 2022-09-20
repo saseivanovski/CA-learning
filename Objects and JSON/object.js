1ec993d8115966ef38a215fe8617840fd5cc72c9
saseivanovski <sase.ivanovski@gmail.com>
2022-09-20 23:48:25 +0200

    homework

--- Objects and JSON/object.js
+++ Objects and JSON/object.js
@@ -11,3 +11,33 @@ const me = {
 };
 const entries = Object.entries(me);
 console.log(entries);
+
+// 2.
+const you = ["he", "she", "it"];
+console.log(Object.assign({ ...you }));
+
+// 3.
+const one = {
+  name: 1,
+  age: 2,
+};
+const two = {
+  name: 1,
+  age: 2,
+};
+
+// easy solutiion for checking if the values of 2 objects are same
+// console.log(JSON.stringify(one) === JSON.stringify(two));
+
+const isEqual = (one, two) => {
+  const obOne = Object.keys(one);
+  const obTwo = Object.keys(two);
+
+  for (let x of obOne) {
+    if (one[x] !== two[x]) {
+      return false;
+    }
+  }
+  return true;
+};
+console.log(isEqual(one, two));
